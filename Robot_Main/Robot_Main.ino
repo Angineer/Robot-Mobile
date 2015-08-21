c84f8f51765107a4c3e7e6945f3a60f0387290ca
Andy Tracy <Poppyseedling@gmail.com>
2015-08-20 21:36:56 -0400

    Adjusted to have the basic driving algorithm I was thinking of. It's very cautious right now. Will need some tweaking in the future.

--- Robot_Main/Robot_Main.ino
+++ Robot_Main/Robot_Main.ino
@@ -16,61 +16,68 @@ const int leftb=5;
 
 //Steering variables
 Servo steer;
-const int servopin=9;
+const int servopin=10;
 
 //Eyeball variables
 Servo look;
-const int lookpin=8;
+const int lookpin=11;
 const int pingPin = 7;
-long duration, distance, lastdist = 30;
+long duration, lastdist = 30;
+int distVector[]={30, 30, 30};
 
 void setup(){
+  Serial.begin(9600); // For debugging
   pinMode(rightf, OUTPUT); //Set motor control pins to output mode
   pinMode(rightb, OUTPUT);
   pinMode(leftf, OUTPUT);
   pinMode(leftb, OUTPUT);
-  pinMode(servopin, OUTPUT);
+  pinMode(servopin, OUTPUT); // Set servo control pins to output mode
   pinMode(lookpin, OUTPUT);
-  DriveStop();
+  DriveStop(); // Start with all motors off
   
-  steer.attach(servopin); //Attach servo
-  steer.writeMicroseconds(1500); //Start servo at 90 degrees
-  look.attach(lookpin); //Attach servo
-  look.writeMicroseconds(1500); //Start servo at 90 degrees
+  steer.attach(servopin); //Attach drive servo
+  Turn(90); //Start servo at 90 degrees
+  look.attach(lookpin); //Attach eyeball servo
+  Look(90); //Start servo at 90 degrees
 }
 
 void loop(){
+  //while(1){}
   //Look around
-  Look(70);
-  Look(110);
+  Look(55);
+  distVector[0]=GetDist();
+  Look(130);
+  distVector[1]=GetDist();
   Look(90);
+  distVector[2]=GetDist();
   
-  //Send out a ping
-  //duration=SendPing();
-  duration=1000;
-  
-  //Figure out how far we are from an object (in inches)
-  distance = duration / 73.746 / 2;
-  //if (distance > 60) distance=lastdist;
+  Serial.print("Distance: ");
+  Serial.print(distVector[0]);
+  Serial.print(distVector[1]);
+  Serial.println(distVector[2]);
   
   //Drive accordingly
-  if (distance < 18){
+  if (distVector[1] < 10
+        || distVector[0] < 10
+        || distVector[2] < 10){
     DriveBackward();
     Turn(60);
-    delay(1500);
+    delay(500);
     DriveForward();
     Turn(90);
   }
   else{
     DriveForward();
+    //DriveStop();
   }
 
-  //Remember the last distance and wait to take a new reading  
-  lastdist=distance;
-  delay(500);
+  //Drive very cautiously 
+  delay(150);
+  DriveStop();
+  delay(350);
 }
 
-long SendPing(){
+long GetDist(){
   pinMode(pingPin, OUTPUT);
   digitalWrite(pingPin, LOW);
   delayMicroseconds(2);
@@ -80,7 +87,10 @@ long SendPing(){
   
   //Pick up the ping
   pinMode(pingPin, INPUT);
-  return pulseIn(pingPin, HIGH);
+  duration=pulseIn(pingPin, HIGH);
+  
+  //Convert to inches
+  return duration / 73.746 / 2;
 }
 
 void Look(int pos){ //Turns the servo to the given angle in degrees
@@ -90,12 +100,16 @@ void Look(int pos){ //Turns the servo to the given angle in degrees
     look.writeMicroseconds(pos); //Send it to the servo
     delay(150);
   }
+  //Pause for debugging
+  delay(250);
 }
 
 void Turn(int pos){ //Turns the servo to the given angle in degrees
-  pos=pos*10+685; //Convert angle to microseconds
+  pos=pos*10+580; //Convert angle to microseconds
+  Serial.println(pos);
   
   if(pos>1100 && pos<=2100){ //If the angle is acceptable
+    Serial.println("Turning!");
     steer.writeMicroseconds(pos); //Send it to the servo
     delay(150);
   }
