1df1003f5fd2a6170ee00f2d17f02ed11826a8ab
Andy Tracy <adtme11@gmail.com>
2019-11-05 07:20:45 -0500

    Finish reworking arduino sketch; still needs testing

--- arduino/robot_main.ino
+++ arduino/robot_main.ino
@@ -12,71 +12,63 @@
   Author: Andy Tracy <adtme11@gmail.com>
 */
 
-#define CBUFFER_SIZE 100
-#define MOVING_AVG_WINDOW_SIZE 4
-#define MIN_OBS_DIST 6 // Minimum obstacle distance in inches
-
-#define DEBUG_MODE 1
-
 #include "Wire.h"
 #include "sensorbar.h"
 #include <Servo.h>
 
-// General variables
+/*** Configuration ***/
+// Motor control
+#define RIGHT_FWD_PIN  3
+#define RIGHT_BACK_PIN 2
+#define LEFT_FWD_PIN   5
+#define LEFT_BACK_PIN  4
+#define MAX_SPEED 100 // Max speed for forward driving, 0-255
+
+// Eyeball sensor
+#define LOOK_PIN 11
+#define PING_PIN 7
+#define MIN_OBS_DIST 6 // Minimum obstacle distance in inches
+#define EYEBALL_MOVE_TIME 150 // Time for servo to reach desired position in ms
+
+// Line sensor
+#define MOVING_AVG_WINDOW_SIZE 4
+#define CIRCULAR_BUFFER_SIZE 100
+
+/*** Global vars ***/
+// The high-level instruction from the raspberry pi about what we should be
+// doing. 'h' = halt, 'd' = drive
 char directive { 'h' };
 
-// Drive motor variables
-// High on rightf = forward on right, etc.
-const int rightf = 3;
-const int rightb = 2;
-const int leftf = 5;
-const int leftb = 4;
-int baseSpeed = 100; // Max speed for forward driving, 0-255
-
 // Line sensor variables
-const uint8_t SX1509_ADDRESS = 0x3E;  // SX1509 I2C address (00)
-SensorBar mySensorBar ( SX1509_ADDRESS );
-CircularBuffer positionHistory ( CBUFFER_SIZE );
-int16_t avePos;
-float avePos_float;
+SensorBar mySensorBar ( 0x3E ); // SX1509 I2C address (00)
+CircularBuffer positionHistory ( CIRCULAR_BUFFER_SIZE );
 
 // Eyeball variables
 Servo look;
-const int lookpin = 11;
-const int pingPin = 7;
-long duration, lastdist = 30;
+int eyeballArray[] = { 30, 30, 30 }; // Left, center, right distances in inches
 
-// The left, center, and right distance readings from the eyeball sensor, in
-// inches
-int sensorArray[] = { 30, 30, 30 };
 
 void setup(){
     Serial.begin ( 9600 ); // Connect to raspi
 
     // Motor control
-    pinMode ( rightf, OUTPUT );
-    pinMode ( rightb, OUTPUT );
-    pinMode ( leftf, OUTPUT );
-    pinMode ( leftb, OUTPUT );
-    pinMode ( lookpin, OUTPUT );
+    pinMode ( RIGHT_FWD_PIN, OUTPUT );
+    pinMode ( RIGHT_BACK_PIN, OUTPUT );
+    pinMode ( LEFT_FWD_PIN, OUTPUT );
+    pinMode ( LEFT_BACK_PIN, OUTPUT );
     DriveStop(); // Start with all motors off
 
     // Eyeball sensor
-    look.attach ( lookpin ); //Attach eyeball servo
-    Look ( 90 ); //Start servo at 90 degrees
+    pinMode ( LOOK_PIN, OUTPUT );
+    look.attach ( LOOK_PIN );
+    rotateEyeballServo ( 90 ); // Start servo at 90 degrees
 
     // Line sensor
-    // The IR will only be turned on during reads.
-    mySensorBar.setBarStrobe();
-    //Other option: Command to run all the time
-    //mySensorBar.clearBarStrobe();
-
-    //Default dark on light
-    mySensorBar.clearInvertBits();
-    //Other option: light line on dark
-    //mySensorBar.setInvertBits();
-
-    uint8_t returnStatus = mySensorBar.begin();
+    mySensorBar.setBarStrobe(); // Turn on IR only during reads
+    //mySensorBar.clearBarStrobe(); // Run IR all the time
+    mySensorBar.clearInvertBits(); // Dark line on light
+    //mySensorBar.setInvertBits(); // Light line on dark
+    mySensorBar.begin();
 }
 
 void loop(){
@@ -88,41 +80,48 @@ void loop(){
     // Proceed based on the current directive from the raspi. 
     if ( directive == 'h' ) { // If we are halted, just hang out.
     } else if ( directive == 'd' ) { // Raspi says we need to get moving
-        // Take readings from all the sensors
+        // Check for obstacles detected by the eyeball sensor
+        readEyeballSensor ( eyeballArray );
+        for ( int i = 0; i < 3; ++i ) {
+            if ( eyeballArray[i] < MIN_OBS_DIST ) {
+                DriveStop();
+                return;
+            }
+        }
+
+        // Check our position against the line
         float linePos = readLineSensor();
-        int eyeballArray[] = readEyeballSensor();
+        float linear = MAX_SPEED;
+        float angular = linePos;
 
-        // First, grab data from the line sensor and save it to the circular buffer
-        //int temp = mySensorBar.getDensity();
-        //if( (temp < 4)&&(temp > 0) )
-        //if (temp < 4)
-        //{
-            //positionHistory.pushElement ( mySensorBar.getPosition() );
-        //}
-        // Get an average of the last n readings
-        //avePos = positionHistory.averageLast ( MOVING_AVG_WINDOW_SIZE );
-
-        // Next, grab readings from the eyeball sensor
-        //Look(45);
-        //sensorArray[0]=GetDist();
-        //Look(135);
-        //sensorArray[2]=GetDist();
-        //Look(90);
-        //sensorArray[1]=GetDist();
-
-        // Drive based on sensor readings
-        FollowLine ( linePos, eyeballArray );
+        drive ( linear, angular );
     }
 
     // Don't let the loop run too fast
     delay ( 150 );
 }
 
+/* 
+ * @brief Read the line sensor array
+ * @return The average of the last MOVING_AVG_WINDOW_SIZE position readings
+ */
+float readLineSensor()
+{
+    // First, grab data from the line sensor and save it to the circular buffer
+    int temp = mySensorBar.getDensity();
+    if ( ( temp < 4 ) && ( temp > 0 ) )
+    {
+        positionHistory.pushElement ( mySensorBar.getPosition() );
+    }
+    // Get an average of the last n readings
+    return float ( positionHistory.averageLast ( MOVING_AVG_WINDOW_SIZE ) );
+}
+
 /*
  * @brief Get a single distance reading from the Ping ultrasonic sensor
  * @return The distance measured by the sensor in inches
  */
-long readEyeballSensor()
+long ping()
 {
   pinMode(pingPin, OUTPUT);
   digitalWrite(pingPin, LOW);
@@ -130,13 +129,29 @@ long readEyeballSensor()
   digitalWrite(pingPin, HIGH);
   delayMicroseconds(5);
   digitalWrite(pingPin, LOW);
-  
+
   // Pick up the ping
-  pinMode(pingPin, INPUT);
-  duration=pulseIn(pingPin, HIGH);
-  
+  pinMode ( pingPin, INPUT );
+  long duration = pulseIn ( pingPin, HIGH );
+
   // Convert to inches
   return duration / 73.746 / 2;
+}
+
+/* 
+ * @brief Take three readings from the ping sensors at different angles (left,
+ *        center, and right)
+ * @param outputArray An array into which the reading will be saved
+ */
+void readEyeballSensor ( long* outputArray )
+{
+    // Grab left, right, and center readings from the eyeball sensor
+    rotateEyeballServo ( 45 );
+    outputArray[0] = ping();
+    rotateEyeballServo ( 135 );
+    outputArray[2] = ping();
+    rotateEyeballServo ( 90 );
+    outputArray[1] = ping();
 }
 
 /*
@@ -149,41 +164,40 @@ void rotateEyeballServo ( int pos )
   
   if ( pos>1000 && pos<=2100 ) { // If the angle is acceptable
     look.writeMicroseconds ( pos ); // Send it to the servo
-    delay ( 150 );
+    delay ( EYEBALL_MOVE_TIME );
   }
 }
 
 /*
  * @brief Drive the robot
- * @param linearSpeed Linear speed of the robot from 0.0 to 1.0. Positive is
+ * @param linearSpeed Linear speed of the robot from -1.0 to 1.0. Positive is
  *        forward, negative is backward.
- * @param angularSpeed Angular speed of the robot from 0.0 to 1.0. Positive is
+ * @param angularSpeed Angular speed of the robot from -1.0 to 1.0. Positive is
  *        left, negative is right.
  */
 void drive ( float linearSpeed, float angularSpeed )
 {
-  analogWrite ( rightf, baseSpeed * ( right / 100.0 ) );
-  digitalWrite(rightb, LOW);
-  analogWrite(leftf, baseSpeed*(left/100.0));
-  digitalWrite(leftb, LOW);
-}
+    bool sign = linearSpeed >= 0;
 
-void driveBackward(int angle){ //Drive backward
-  digitalWrite(rightf, LOW);
-  digitalWrite(leftf, LOW);
-  
-  if (angle==0){
-    digitalWrite(rightb, HIGH);
-    digitalWrite(leftb, HIGH);
-  }
-  else if (angle>0){
-    digitalWrite(rightb, HIGH);
-    digitalWrite(leftb, LOW);
-  }
-  else{
-    digitalWrite(rightb, LOW);
-    digitalWrite(leftb, HIGH);
-  }
+    if ( angularSpeed => 0 ) {
+        leftSpeed = ( 1.0 - angularSpeed ) * MAX_SPEED;
+        rightSpeed = MAX_SPEED;
+    } else {
+        leftSpeed = MAX_SPEED;
+        rightSpeed = ( 1.0 + angularSpeed ) * MAX_SPEED;
+    }
+
+    if ( linearSpeed >= 0 ) {
+        analogWrite ( leftf, linearSpeed * leftSpeed );
+        digitalWrite ( leftb, LOW );
+        analogWrite ( rightf, linearSpeed * rightSpeed );
+        digitalWrite ( rightb, LOW );
+    } else {
+        digitalWrite ( leftf, LOW );
+        analogWrite ( leftb, -linearSpeed * leftSpeed );
+        digitalWrite ( rightf, LOW );
+        analogWrite ( rightb, -linearSpeed * rightSpeed );
+    }
 }
 
 /*
@@ -195,34 +209,5 @@ void driveStop()
   digitalWrite(rightb, LOW);
   digitalWrite(leftf, LOW);
   digitalWrite(leftb, LOW);
-}
-
-void FollowLine(){
-    // Check for obstacles detected by the eyeball sensor
-    if ( DEBUG_MODE ) {
-        Serial.println ( "Checking for obstacles..." );
-    }
-
-    for ( int i = 0; i < 3; ++i ) {
-        if ( sensorArray[i] < MIN_OBS_DIST ) {
-            DriveStop();
-            return;
-        }
-    }
-
-    if ( DEBUG_MODE ) {
-        Serial.println ( "No obstacles detected, driving forward" );
-    }
-
-    // If no obstacles were detected, drive along the line
-    if (avePos < -50){
-        DriveForward(0, 100);
-    }
-    else if (avePos > -50 && avePos < 50){
-        DriveForward(100, 100);
-    }
-    else if (avePos > 50){
-        DriveForward(100, 0);
-    }
 }
 
