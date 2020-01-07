daf90f817542502feca99723958b62468d948a25
Andy Tracy <adtme11@gmail.com>
2020-01-07 02:19:54 +0000

    Add script for grabbing image from camera

--- src/mobile_server.cpp
+++ src/mobile_server.cpp
@@ -1,12 +1,12 @@
 #include "MobileManager.h"
 
-#include <iostream>            
+#include <iostream>
 
 int main ( int argc, char *argv[] ) 
 { 
     std::cout << "Starting Mobile Manager" << std::endl;
 
-    // Create the manager      
+    // Create the manager
     MobileManager manager;
 
     // Run forever
