31d9fb3daf4448560704cea031673e57bf53abf4
Andy Tracy <adtme11@gmail.com>
2020-04-29 03:49:27 +0100

    Copy code over to camera create/destroy functions

--- raspicam/RaspiCam.h
+++ raspicam/RaspiCam.h
@@ -1,4 +1,11 @@
+#include "RaspiStill.h"
+
 /*!
- * @brief Create a new camera object
+ * @brief Create a new camera object and return a pointer to it
  */
-void createCam();
+RASPISTILL_STATE* createCam ( int argc, const char** argv );
+
+/*!
+ * @brief Clean up a camera object
+ */
+void destroyCam ( RASPISTILL_STATE* state );
