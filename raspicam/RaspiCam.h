82add47097626504c168f046044be773dad4defb
Andy Tracy <adtme11@gmail.com>
2020-05-04 02:24:58 +0100

    Create capture function

--- raspicam/RaspiCam.h
+++ raspicam/RaspiCam.h
@@ -1,7 +1,9 @@
 #include "RaspiStill.h"
 
 /*!
- * @brief Create a new camera object and return a pointer to it
+ * @brief Create a new camera object and return a pointer to it. Accepts the
+ *        same command line arguments as the raspistill app, if you feel so
+ *        inclined.
  */
 RASPISTILL_STATE* createCam ( int argc, const char** argv );
 
@@ -9,3 +11,8 @@ RASPISTILL_STATE* createCam ( int argc, const char** argv );
  * @brief Clean up a camera object
  */
 void destroyCam ( RASPISTILL_STATE* state );
+
+/*!
+ * @brief Capture a frame from the camera and save it to memeory
+ */
+void capture ( RASPISTILL_STATE* state );
