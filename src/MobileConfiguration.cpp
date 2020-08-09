bfbf649b34ecfbbd17683fbf48586f9f562b7547
Andy Tracy <adtme11@gmail.com>
2020-08-09 16:59:17 +0000

    Install binary and add a service

--- src/MobileConfiguration.cpp
+++ src/MobileConfiguration.cpp
@@ -4,7 +4,7 @@ MobileConfiguration::MobileConfiguration()
 {
     configMap.emplace ( "home_id", "0" );
     configMap.emplace ( "couch_id", "1" );
-    configMap.emplace ( "sleep_time_s", "5" );
+    configMap.emplace ( "sleep_time_s", "10" );
     configMap.emplace ( "img_width", "640" );
     configMap.emplace ( "img_height", "480" );
 }
